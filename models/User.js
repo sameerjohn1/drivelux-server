27a002b432003f84adc2bdce9ee183d46c1b5b2a
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-06-04 12:33:52 +0500

    password hashing applied

--- models/User.js
+++ models/User.js
@@ -36,8 +36,13 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
